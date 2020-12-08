8a2909570ff673e8e9fa377b30e531bde10eca15
diogo07 <diogosousa36@gmail.com>
2020-12-08 14:48:39 -0300

    add inputs on function config

--- genetic_algorithms.py
+++ genetic_algorithms.py
@@ -8,26 +8,28 @@ from deap import algorithms
 
 class GeneticAlgorithms:
 
+    def __init__(self):
+        self.toolbox = None
+
     def configFlipBitTournament(self, typeRandom, minValue, maxValue, evaluate, weights, size, mate):
         creator.create("FitnessType", base.Fitness, weights=weights)
         creator.create("Individual", list, fitness=creator.FitnessType)
-        toolbox = base.Toolbox()
-        toolbox.register("attr", typeRandom, minValue, maxValue)
-        toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr, size)
-        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+        self.toolbox = base.Toolbox()
+        self.toolbox.register("attr", typeRandom, minValue, maxValue)
+        self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr, size)
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
 
-        toolbox.register("evaluate", evaluate)
+        self.toolbox.register("evaluate", evaluate)
 
-        toolbox.register("mate", mate)
-        toolbox.register("mutate", tools.mutFlipBit, indpb=0.1)
-        toolbox.register("select", tools.selTournament, tournsize=3)
+        self.toolbox.register("mate", mate)
+        self.toolbox.register("mutate", tools.mutFlipBit, indpb=0.1)
+        self.toolbox.register("select", tools.selTournament, tournsize=3)
 
-        return toolbox
 
-    def start(self, toolbox, population, hallOfFame):
+    def start(self, population, hallOfFame):
         random.seed(64)
 
-        pop = toolbox.population(n=population)
+        pop = self.toolbox.population(n=population)
         hof = tools.HallOfFame(hallOfFame)
         stats = tools.Statistics(lambda ind: ind.fitness.values)
         stats.register("avg", numpy.mean)
@@ -35,7 +37,7 @@ class GeneticAlgorithms:
         stats.register("min", numpy.min)
         stats.register("max", numpy.max)
 
-        pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=3000,
+        pop, log = algorithms.eaSimple(pop, self.toolbox, cxpb=0.5, mutpb=0.2, ngen=3000,
                                        stats=stats, halloffame=hof, verbose=True)
 
         return pop, log, hof
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     ag = GeneticAlgorithms()
 
-    toolbox = ag.configFlipBitTournament(random.randint, 0, 1, chuang_f1, (-1.0,), 10, tools.cxTwoPoint)
-    results = ag.start(toolbox, 50, 1)
+    ag.configFlipBitTournament(random.randint, 0, 1, chuang_f1, (-1.0,), 10, tools.cxTwoPoint)
+    results = ag.start(50, 1)
 
     plotChart(results[1].select('gen'), results[1].select('min'))
